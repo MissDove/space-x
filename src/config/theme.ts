13ca5ff99210df49827e95efe8d8476dc3529c6b
Agata Jones <28706207+MissDove@users.noreply.github.com>
2021-03-28 19:47:07 +0100

    Add colours (#2)

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -2,6 +2,16 @@ export const Theme = {
     fontFamily: "Helvetica, sans-serif",
 
     // colours
+    textDark: "#392D40",
+    textLight: "#B9AEAC",
+
+    primaryRed: "#E75353",
+
+    primaryYellow: "#F6D586",
+
+    primaryBlue: "#5A9DB4",
+
+    backgroundColor: "#FDFFF6",
 
     // border radius
     radius1: "4px",
